f13d72bc9e0817639926e47a3ec82f343a23d73b
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-07-19 19:44:00 +0700

    update

---
+++ db-demo/src/main/java/com/SpringBoot/dbdemo/DbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.dbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbDemoApplication.class, args);
+	}
+
+}
---
+++ db-demo/src/test/java/com/SpringBoot/dbdemo/DbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.dbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
